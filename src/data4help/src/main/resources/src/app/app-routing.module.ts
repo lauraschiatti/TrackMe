16ecd6e8661ac53f39025fae534a7a120adfc315
lauricdd <lauri_cdd@hotmail.com>
2018-11-25 17:29:35 +0100

    data4helpp frontend: home, login and register. Ignore node_modules.

---
+++ src/data4help/src/main/resources/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: '', pathMatch: 'full' },
+  { path : '', component : HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
+
+
