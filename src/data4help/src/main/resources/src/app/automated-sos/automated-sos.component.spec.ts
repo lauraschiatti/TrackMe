7e54a73b32b3d0299e2a97f055666e6388ba3e92
lauricdd <lauri_cdd@hotmail.com>
2018-12-05 13:03:30 +0100

    update d4h, asos and t4r UIs

---
+++ src/data4help/src/main/resources/src/app/automated-sos/automated-sos.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AutomatedSOSComponent } from './automated-sos.component';
+
+describe('AutomatedSOSComponent', () => {
+  let component: AutomatedSOSComponent;
+  let fixture: ComponentFixture<AutomatedSOSComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AutomatedSOSComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AutomatedSOSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/data4help/src/main/resources/src/app/data4help/data4help.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Data4helpComponent } from './data4help.component';
+
+describe('Data4helpComponent', () => {
+  let component: Data4helpComponent;
+  let fixture: ComponentFixture<Data4helpComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Data4helpComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Data4helpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
