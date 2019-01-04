e3bc22ef3eb74d3fb87aeda3c70e6265196bf2ad
lauricdd <lauri_cdd@hotmail.com>
2019-01-04 10:44:56 +0100

    Individual register form, inputs and validations

--- src/data4help/src/main/resources/src/app/register/register.component.ts
+++ src/data4help/src/main/resources/src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import { HttpClient } from '@angular/common/http';
+
+// import { Router } from '@angular/router';
+// import { IndividualService } from '../_services/individual.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    individualForm: FormGroup;
+    submitted = false;
+    bloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder,
+        // private route: ActivatedRoute,
+        // private router: Router,
+        // private individualnService: IndividualService,
+    ) {
+        // this.individualForm = this.formBuilder.group({
+        //     bloodTypes: this.bloodTypes
+        // });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.individualForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            gender: ['', Validators.required],
+            birthDate: ['', Validators.required],
+            ssn: ['', Validators.required],
+            weight: ['', Validators.required],
+            height: ['', Validators.required],
+            bloodType: ['', Validators.required],
+            address: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+        });
+
+        // this.returnUrl =  '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.individualForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.individualForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.individualForm.value));
+
+        // http.get(baseUrl + 'api/SampleData/GetSummaries').subscribe(result => {
+        //     this.summaries = result.json() as any[];
+        // }, error => console.error(error));
+    }
 
 }
