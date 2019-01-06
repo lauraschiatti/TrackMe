a5073fe7b8ddfec5567675cd38798fd61c7b3aa7
lauricdd <lauri_cdd@hotmail.com>
2019-01-06 16:02:36 +0100

    user and role models

---
+++ src/data4help/src/main/resources/src/app/_models/role.ts
@@ -0,0 +1,4 @@
+export enum Role {
+    Individual = 'Individual',
+    ThirdParty = 'ThirdParty'
+}
---
+++ src/data4help/src/main/resources/src/app/_models/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    userId: number;
+    role: string;
+    accessToken?: string;
+}
