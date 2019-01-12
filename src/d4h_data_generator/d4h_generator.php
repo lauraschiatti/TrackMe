dd119df0e33b338b7a1b9d38f77d46a2c3da5d48
Diego E. Avila <adiego73@gmail.com>
2019-01-12 13:45:33 +0100

    add data_gen Dockerfile and updating docker-compose file in order to run data simulation

--- src/d4h_data_generator/d4h_generator.php
+++ src/d4h_data_generator/d4h_generator.php
@@ -7,9 +7,9 @@ function random_float ($min,$max) {
 }
 
 function generateData(){
-    // Get the contents of the JSON file 
+    // Get the contents of the JSON file
     $coordinatesFileContent = file_get_contents(__DIR__ . "/coordinates.json");
-    // Convert to array 
+    // Convert to array
     $individuals = json_decode($coordinatesFileContent, true);
 
     foreach($individuals as $individual) {
@@ -19,9 +19,9 @@ function generateData(){
             "latitude" => $individual["latitude"],
             "longitude" => $individual["longitude"]
         );
-        
+
         /** HealthStatus **/
-        $heartRate = rand(50, 200);    
+        $heartRate = rand(50, 200);
         $systolic = random_float(108, 140);
         $diastolic = random_float(67, 88);
         $bodyTemperature = random_float(35, 41.5);
@@ -40,26 +40,28 @@ function generateData(){
             "location" => $location,
             "healthStatus" => $healthStatus,
         );
-     
+
         pushData($data);
     }
 }
 
 function pushData($data) {
     //API URL
-    $url = 'http://0.0.0.0:4567/internal/data';
+    $url = 'http://d4h:4567/internal/data';
 
     //create a new cURL resource
     $ch = curl_init($url);
 
     //setup request to send json via POST
-    $payload = json_encode(array("data" => $data));
+    $payload = json_encode($data);
+
+    // var_dump($payload);
 
     //attach encoded JSON string to the POST fields
     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
     //set the content type to application/json
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
     //return response instead of outputting
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -69,12 +71,8 @@ function pushData($data) {
 
     if(curl_errno($ch)) {
         echo "error ".curl_error($ch)."\n";
-        return false;
-    }else{
-        print_r($data);
-        echo "\n";
-        return true;
     }
+    
     $response = curl_exec($ch) ;
 
     //close URL resource
