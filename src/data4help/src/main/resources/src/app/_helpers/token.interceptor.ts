f45d3db1d171970c5317db36056599fc005740c3
lauricdd <lauri_cdd@hotmail.com>
2019-01-13 22:56:57 +0100

    address in search and register forms

--- src/data4help/src/main/resources/src/app/_helpers/token.interceptor.ts
+++ src/data4help/src/main/resources/src/app/_helpers/token.interceptor.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
 
 import {AuthenticationService} from '../_services';
 
@@ -9,7 +8,6 @@ import {AuthenticationService} from '../_services';
 export class TokenInterceptor implements HttpInterceptor {
     constructor(
         private authenticationService: AuthenticationService,
-        private router: Router,
     ) {
     }
 
@@ -19,13 +17,6 @@ export class TokenInterceptor implements HttpInterceptor {
         const currentUser = this.authenticationService.currentUserValue;
 
         if (currentUser && currentUser.accessToken) {
-
-            // check if token is valid check expiration of token before adding as heade
-            // --> catch error  200/403
-            // Check whether the token is expired and return
-            // true or false
-            // return !this.jwtHelper.isTokenExpired(token);
-
             request = request.clone({
                 headers: request.headers
                     .set('ACCESS-TOKEN', currentUser.accessToken)
